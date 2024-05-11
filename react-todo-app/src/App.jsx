b344625116fac8285d6fdb623c6fdc3cdf8711c3
Adam Musial <musialadam83@gmail.com>
2024-05-11 16:39:31 +0800

    feat: add html structure

---
+++ react-todo-app/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+
+  return (
+    <div>
+      <header className="app_header">HEader</header>
+      <main className="app_main">
+        <section className="task_column"></section>
+        <section className="task_column"></section>
+        <section className="task_column"></section>
+      </main>
+    </div>
+
+  )
+}
+
+export default App
