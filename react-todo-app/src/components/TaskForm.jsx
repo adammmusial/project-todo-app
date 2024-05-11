d5517e2ab167fe084b9f74f7a2ab4fe4c76a45ed
Adam Musial <musialadam83@gmail.com>
2024-05-11 18:54:21 +0800

    feat: add styling for task input

--- react-todo-app/src/components/TaskForm.jsx
+++ react-todo-app/src/components/TaskForm.jsx
@@ -1,35 +1,38 @@
 import React, { useState } from "react";
 
 
- 
-function  TaskForm()  {
-	const  [inputValue, setInputValue] =  useState('');
+function TaskForm() {
+    const [inputValue, setInputValue] = useState('');
 
-	const  handleChange = (event) => {
-		setInputValue(event.target.value);
-	};
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+    };
 
-return  (
-    <header className="app_header">
-        <form>
-            <input  type="text"  value={inputValue} onChange={handleChange} placeholder="Input Value" />
-        </form>
-        <div className="task_for_bottom_line">
-            <button className="tag">HTML</button>
-            <button className="tag">JavaScript</button>
-            <button className="tag">CSS</button>
-            <button className="tag">React</button>
-            <button className="tag"></button>
+    return (
+        <header className="app_header">
+            <form>
+                <input type="text" className="task_input" value={inputValue} onChange={handleChange} placeholder="Input Value" />
+            </form>
+            <div className="task_for_bottom_line">
+                <div>
+                    <button className="tag">HTML</button>
+                    <button className="tag">JavaScript</button>
+                    <button className="tag">CSS</button>
+                    <button className="tag">React</button>
+                    <button className="tag"></button>
+                </div>
+                <div>
+                    <select name="" id="" className="task_status">
 
-            <select name="" id="" className="task_status">
+                        <option value="todo">To do</option>
+                        <option value="doing">Doing</option>
+                        <option value="done">Done</option>
 
-                <option value="todo">To do</option>
-                <option value="doing">Doing</option>
-                <option value="done">Done</option>
-            
-            </select>
-            <button type = 'submit' className="task_submit">+ Add Task</button>
-        </div>
-    </header>
-)};
+                    </select>
+                    <button type='submit' className="task_submit">+ Add Task</button>
+                </div>
+            </div>
+        </header>
+    )
+};
 export default TaskForm
