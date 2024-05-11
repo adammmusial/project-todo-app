bcb86858877737ddadf7731b36b1ccff9e50d5e1
Adam Musial <musialadam83@gmail.com>
2024-05-11 17:04:28 +0800

    feat: add flexbox

--- react-todo-app/src/App.jsx
+++ react-todo-app/src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from "react"
 import './App.css'
 
 const App = () => {
 
   return (
-    <div>
+    <div className="app"> 
       <header className="app_header">HEader</header>
       <main className="app_main">
-        <section className="task_column"></section>
-        <section className="task_column"></section>
-        <section className="task_column"></section>
+        <section className="task_column">Section 1</section>
+        <section className="task_column">Section 2</section>
+        <section className="task_column">Section 2</section>
       </main>
     </div>
 
