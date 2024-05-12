3940864ab509fa8ed7e22856eb71cc2f626a3da9
Adam Musial <musialadam83@gmail.com>
2024-05-12 17:55:00 +0800

    feat: add TaskCard component and styling

---
+++ react-todo-app/src/components/TaskCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Tag from './Tag'
+import deleteIcon from '../assets/delete.png'
+import './TaskCard.css'
+
+const TaskCard = ({title}) => {
+  return (
+        <article className="task_card">
+            <p className="task_text">This is simple text</p>
+            <div className='task_card_bottom_line'>
+                <div className="task_tag_cards">
+                    <Tag name="hrml"/>
+                    <Tag name="hrml"/>
+                    <Tag name="hrml"/>
+                </div>
+                <div className="task_delete">
+                    <img src={deleteIcon} className='delete_icon'></img>
+                </div>
+            </div>
+        </article>
+
+  )
+}
+
+export default TaskCard
--- react-todo-app/src/components/TaskColumn.jsx
+++ react-todo-app/src/components/TaskColumn.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import Todo from '../assets/direct-hit.png'
 import './TaskColumn.css'
+import TaskCard from './TaskCard'
 
 export const Column = ({title,icon}) => {
   return (
     <section className="task_column">
         <h2 className='task_column_heading'>
-            <img className='task_column_icon'src={icon} alt={Todo}></img>
+            <img className='task_column_icon'src={icon} alt=''></img>
             {title}
         </h2>
+
+        <TaskCard title="test"/>
     </section>
   )
 }
